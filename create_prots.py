ebb840ebef7b857384bcb37cf648a12f97298b5d
t-taketsune <taketsuki@protonmail.com>
2024-10-07 17:07:38 -0300

    [add] support for front-only and back-only protocols

--- create_prots.py
+++ create_prots.py
@@ -5,20 +5,37 @@ import json
 import argparse
 import os
 
+def get_files(face="both", fdir="rg_dataset/"):
+	if face == "both":
+		ret = [x.split("/")[-1] for x in glob(f"{fdir}wild/*") if not x.endswith(".txt")]
+	elif face == "front":
+		with open(f"{fdir}front_files.txt", "r") as fd:
+			ret = [x.strip().split(" ")[0] for x in fd.readlines()]
+	elif face == "back":
+		with open(f"{fdir}back_files.txt", "r") as fd:
+			ret = [x.strip().split(" ")[0] for x in fd.readlines()]
+
+	return ret
+
 if __name__ == "__main__":
 	ps = argparse.ArgumentParser()
 	ps.add_argument("--nfold", "-n", type=int, default=10)
 	ps.add_argument("--protocol", "-p", type=str, default="closeup")
+	ps.add_argument("--face", "-f", type=str, default="both")
+	ps.add_argument("--dataset_dir", "-d", type=str, default="rg_dataset/")
+	ps.add_argument("--output_dir", "-o", type=str, default="prots/")
 	args = ps.parse_args()
 
 	part = args.protocol
 	n = args.nfold
+	face = args.face
+	dataset_dir = args.dataset_dir
+	output_dir = args.output_dir
 
-	if not os.path.exists("rg_dataset/prots/"):
-		os.mkdir("rg_dataset/prots/")
+	if not os.path.exists("rg_dataset/prots2/"):
+		os.mkdir("rg_dataset/prots2/")
 
-	# This line is just for getting all labels - doesn't matter if from wild or closeup
-	fs = [x.split("/")[-1] for x in glob("rg_dataset/wild/*") if not x.endswith(".txt")]
+	fs = get_files(face=face, fdir=dataset_dir)
 	random.shuffle(fs)
 
 	# Split the list of all files into n bins
@@ -42,11 +59,12 @@ if __name__ == "__main__":
 		train_fls = [x for xs in train_fls for x in xs]
 		#print(ti, vi, len(train_fls))
 	
-		with open(f"rg_dataset/prots/{part}_prot{i+1}_{n}.json", "w") as fd:
+		with open(f"{dataset_dir}/{output_dir}/{part}_{face}_prot{i+1}_of_{n}.json", "w") as fd:
 			json.dump({
-				"prefix": f"rg_dataset/{part}/",
+				"prefix": f"{dataset_dir}/{part}/",
 				"train": train_fls,
 				"valid": valid_fl,
 				"test": test_fl
 			}, fd, indent=2, ensure_ascii=False)
+	print(f"Saved {n}-fold protocols at {dataset_dir}/{output_dir}")
 
